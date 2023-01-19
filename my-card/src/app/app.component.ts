2cf1f5cffb363a0e00a5b9c08423719eaa7303de
kasidit bualoeng <kbualoeng@nexient.com>
2023-01-19 12:06:12 -0500

    created posts in app component

--- my-card/src/app/app.component.ts
+++ my-card/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-card';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree_2.png',
+      userName: 'forest',
+      content: 'I saw such a beautiful tree today'
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/mountainbike.png',
+      userName: 'Mountain biking lovers',
+      content: 'I ride a bike today in cthe forest'
+    },
+    {
+      title: 'Snowy Moutian',
+      imageUrl: 'assets/mountain.png',
+      userName: 'Snowy Mountain',
+      content: 'No moutain high enough'
+    },
+    {
+      title: 'Moutian bike again',
+      imageUrl: 'assets/biking.png',
+      userName: 'Snowy Mountain',
+      content: 'No moutain high enough'
+    }
+  ]
 }
